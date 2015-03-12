036188233caac9ef94e604ff13d794b2a193389a
Ivan Arcuschin <iarcuschin@gmail.com>
2015-03-12 12:28:54 -0300

    Moved code to src/rocon_authorization

--- src/rocon_authorization/manager.py
+++ src/rocon_authorization/manager.py
@@ -92,13 +92,13 @@ class UsersManager(object):
     ##########################################################################
 
     def _ros_service_get_users_roles(self, request):
-        '''
+         '''
           Get roles assigned to a user.
 
           @param request user's name
           @type auhtorization_srvs.GetUsersRolesRequest
         '''
-        user_name = request.name
+       user_name = request.name
         if user_name == '':
             rospy.logerr("Users: received request for roles with empty user's name")
             role_list = []
